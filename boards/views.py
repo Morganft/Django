0fb0720419c3e9897c011dc5515554b4e404a777
Nikita Podkhvatov <nikita-podkhvatoff@yandex.ru>
2022-09-28 10:47:36 +0300

    Require login when create new topic

--- boards/views.py
+++ boards/views.py
@@ -3,6 +3,7 @@ from django.http import Http404
 from .forms import NewTopicForm
 from .models import Board, Topic, Post
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
@@ -15,21 +16,21 @@ def board_topics(request, pk):
     board = get_object_or_404(Board, pk=pk)
     return render(request, 'topics.html', {'board': board})
 
+@login_required
 def new_topic(request, pk):
     board = get_object_or_404(Board, pk=pk)
-    user = User.objects.first()
 
     if request.method == 'POST':
         form = NewTopicForm(request.POST)
         if form.is_valid():
             topic = form.save(commit = False)
             topic.board = board
-            topic.starter = user
+            topic.starter = request.user
             topic.save()
             post = Post.objects.create(
                 message = form.cleaned_data.get('message'),
                 topic = topic,
-                created_by = user
+                created_by = request.user
             )
 
             return redirect('board_topics', pk = board.pk)
