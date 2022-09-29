8dd667900f7e247fdb803e1acdce8706750e0cd0
Nikita Podkhvatov <nikita-podkhvatoff@yandex.ru>
2022-09-29 14:14:26 +0300

    Add edit view

--- boards/views.py
+++ boards/views.py
@@ -1,7 +1,11 @@
+from django.db.models import Count
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import NewTopicForm, PostForm
 from .models import Board, Topic, Post
 from django.contrib.auth.decorators import login_required
+from django.views.generic import UpdateView
+from django.utils import timezone
+from django.utils.decorators import method_decorator
 # Create your views here.
 
 
@@ -12,7 +16,8 @@ def home(request):
 
 def board_topics(request, pk):
     board = get_object_or_404(Board, pk=pk)
-    return render(request, 'topics.html', {'board': board})
+    topics = board.topics.order_by('-last_updated').annotate(replies=Count('posts') - 1)
+    return render(request, 'topics.html', {'board': board, 'topics': topics})
 
 @login_required
 def new_topic(request, pk):
@@ -38,6 +43,8 @@ def new_topic(request, pk):
 
 def topic_posts(request, board_pk, topic_pk):
     topic = get_object_or_404(Topic, board__pk=board_pk, pk=topic_pk)
+    topic.views += 1
+    topic.save()
     return render(request, 'topic_posts.html', {'topic': topic})
 
 @login_required
@@ -56,5 +63,23 @@ def reply_topic(request, board_pk, topic_pk):
     return render(request, 'reply_topic.html', {'topic': topic, 'form': form})
 
 
+@method_decorator(login_required, name='dispatch')
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = ('message', )
+    template_name = 'edit_post.html'
+    pk_url_kwarg = 'post_pk'
+    context_object_name = 'post'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(created_by=self.request.user)
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.updated_by = self.request.user
+        post.updated_at = timezone.now()
+        post.save()
+        return redirect('topic_posts', board_pk=post.topic.board.pk, topic_pk=post.topic.pk)
 
 
